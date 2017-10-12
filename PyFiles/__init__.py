ae87c52d7137be3af1714f7db15d9e89e3e886e9
Angus Goody <angus.goody@gmail.com>
2017-10-12 15:10:30 +0100

    Added Basic Window

---
+++ PyFiles/__init__.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+#Angus Goody
+#12/10/17
+#PyPassword 3 Main File
+
+"""
+This is the main file for PyPassword
+3 and contains the core code to run
+PyPassword 3
+"""
+
+
+#====================Imports====================
+from tkinter import *
+from tkinter import messagebox
+
+#====================Window====================
+window=Tk()
+window.title("PyPassword 3")
+window.geometry("400x300")
+
+
+#====================END====================
+window.mainloop()
