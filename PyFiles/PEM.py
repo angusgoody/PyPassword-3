b33458aff3b036d0017e4e36b747bd9dabfd0678
Angus Goody <angus.goody@gmail.com>
2017-10-12 14:53:26 +0100

    Added Python Package files

---
+++ PyFiles/PEM.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+#Angus Goody
+#12/10/17
+#PyPassword 3 Pod Encryption Module
+
+"""
+This file is the encryption module
+and all decrypting and encrypting of 
+data takes place here.
+"""
+
+#==========IMPORTS==========
---
+++ PyFiles/PyUi.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+#Angus Goody
+#12/10/17
+#PyPassword 3 User Interface Module
+
+"""
+This file is the user interface module
+and is where all the user interface elements 
+are created and used.
+"""
+
+#==========IMPORTS==========
